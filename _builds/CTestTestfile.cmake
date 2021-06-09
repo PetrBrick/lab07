1bc9ffd20d7869dc2c497a2b9c6f275b3f8d985c
PetrBrick <petrbrick@gmail.com>
2021-06-09 08:38:24 +0300

    :Perezaliv

---
+++ _builds/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/petr/PetrBrick/workspace/projects/lab07
+# Build directory: /home/petr/PetrBrick/workspace/projects/lab07/_builds
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(check "/home/petr/PetrBrick/workspace/projects/lab07/_builds/check")
+set_tests_properties(check PROPERTIES  _BACKTRACE_TRIPLES "/home/petr/PetrBrick/workspace/projects/lab07/CMakeLists.txt;64;add_test;/home/petr/PetrBrick/workspace/projects/lab07/CMakeLists.txt;0;")
